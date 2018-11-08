ce38f86ce7d8048e306e0b3823c5496b0ca9bfb4
mnicholas26 <spiritbeast26@yahoo.co.uk>
2018-11-08 06:33:37 +0000

    starting word sanitation

--- arrowwords.js
+++ arrowwords.js
@@ -1,4 +1,29 @@
 window.onload = function()
 {
     console.log("skeleton setup");
+
+    //words file parsing and creation
+    var words = text.split('\n');
+    var newwords = [];
+    for(let i = 0; i < words.length; i++)
+    {
+        let parts = words[i].split(' ');
+        let word = {};
+        if(parts[3] == undefined) continue;
+        let neaten = parts[2].slice(1,-1);
+        word.word = neaten;
+        let clue = "";
+        for(let j = 3; j < parts.length; j++)
+        {
+            clue = clue + " " + parts[j];
+        }
+        clue = clue.trim();
+        word.clue = clue;
+        newwords.push(word);
+    }
+
+    var output = document.getElementById('output');
+    output.width = "800px";
+    output.value = JSON.stringify(newwords);
+
 }
